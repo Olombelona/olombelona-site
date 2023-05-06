6ff75e0fde720f7ee7f07b26fdddcaa5850623b0
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2023-05-06 01:11:13 +0100

    active favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,23 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `olombelona-site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.olombelona.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Olombelona",
+        short_name: "Olombelona",
+        start_url: "/",
+        icon: "medias/icon.png", // This path is relative to the root of the site.
+      },
+    },
+  ],
 }
 
 export default config
